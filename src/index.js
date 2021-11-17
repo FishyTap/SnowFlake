672655b82667ccecfc1152190788370f23ea45f0
FishyTap <fishytapnetworks@gmail.com>
2021-11-17 21:33:51 +0800

    Change Testing Variable

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ client.snipes = new Collection();
 
 require(path.join(__dirname, "./loaders/loader"))(client);
 
-const test = true;
+const test = false;
 
 client.login(test == false ? process.env.TOKEN : process.env.TEST);
 
